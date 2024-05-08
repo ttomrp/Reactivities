3e230648084f474cdedc538acbe4cd43d6fbc2d0
ttomrp <ttomrp@gmail.com>
2024-05-08 14:05:06 -0500

    skeleton front end styling

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,37 +1,29 @@
-
-import { Fragment, useEffect, useState } from 'react'
-import axios from 'axios';
-import { Container, Header, List } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
-import NavBar from './NavBar';
+import { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import { Container, Header, List } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
-
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
     axios
-    .get<Activity[]>('http://localhost:5000/api/activities')
-    .then(response => {
-      setActivities(response.data)
-    })
-  }, []) // empty array means useEffect is only called once and doesn't change
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
+  }, []); // empty array means useEffect is only called once and doesn't change
 
   return (
     <Fragment>
-      <NavBar/>
-      <Container style={{marginTop: '7em'}}>
-        <List>
-          {activities.map(activity => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-        </List>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard activities={activities} />
       </Container>
-      
     </Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
